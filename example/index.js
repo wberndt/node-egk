af7e49af1e3ef309049180c1bc07696d7b37d215
Wolfgang Berndt <w.berndt@berndt-edv.de>
2021-02-12 15:08:39 +0100

    First commit, wip...

---
+++ example/index.js
@@ -0,0 +1,20 @@
+const eGKReader = require('../lib/egkreader');
+
+const egk = new eGKReader();
+egk.on('reader-connect', (reader) => { console.log(`Reader connected: ${reader}`) });
+egk.on('reader-disconnect', (reader) => { console.log(`Reader disconnected: ${reader}`) });
+egk.on('card-disconnect', (reader) => { console.log(`Card disconnected from reader: ${reader}`) });
+egk.on('card-connect', async (reader) => {
+    console.log(`Card connected to reader: ${reader}, reading data...`);
+    try {
+        const result = await egk.getPatientData({returnXML: true});
+        console.log("Got data: ", result);
+        //egk.dispose();
+    } catch (err) {
+        console.log("Got error: ", err);
+    }
+
+});
+
+egk.on('error', (err) => { console.log(`Error: ${err.message}`, err) });
+
